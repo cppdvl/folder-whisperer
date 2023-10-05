8aa38b07f6b41bc0e763afab6d8df455c662e403
JG "The Cpp Devil <56121651+cppdvl@users.noreply.github.com>
2023-10-05 18:59:06 -0500

    Create spawner.sh
    This is the initial version.

---
+++ spawner.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Default values
+Backends=("Backend0" "Backend1" "Backend2")
+WrapperName="YourWrapper"
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -b|--backends)
+      # Set the list of backends from the provided argument
+      shift
+      Backends=("$@")
+      shift
+      ;;
+    -w|--wrapper)
+      # Set the wrapper name from the provided argument
+      shift
+      WrapperName="$1"
+      shift
+      ;;
+    *)
+      # Unknown argument
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+# Create the main folder structure
+mkdir -p $WrapperName
+cd $WrapperName
+
+# Create CMakeLists.txt
+echo "project($WrapperName)" > CMakeLists.txt
+
+# Create the 'inc' folder and the wrapper header file
+mkdir -p inc
+touch inc/${WrapperName}Wrapper.h
+
+# Create the 'backends' folder and subfolders for each backend
+mkdir -p backends
+for backend in "${Backends[@]}"; do
+  mkdir -p backends/$backend/rpo
+  mkdir -p backends/$backend/examples
+done
+
+# Exit the 'inc' folder
+cd ..
